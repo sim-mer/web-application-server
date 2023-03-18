222d1ec659e081268d38b6ab4104ddee82c164ad
Gyumin Sim <gyu2781@gmail.com>
2023-03-19 01:10:13 +0900

    요구사항 5 완료

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -37,21 +37,28 @@ public class RequestHandler extends Thread {
             }
             log.debug("request line : {}", line);
             url = HttpRequestUtils.parseLine(line);
+            //방식을 바꿔서 첫 라인만 parse를 하고 나머지는 헤더를 다 읽는걸 기본으로 하는 코드로?
 
             byte[] body = null;
             DataOutputStream dos = new DataOutputStream(out);
-            if (url[0].equals("GET"))
+            if (url[0].equals("GET")) {
                 body = requestGet(url[1]);
+                if (url[1].equals("/index.html")) {
+                    while (!(line = br.readLine()).equals("")) {
+                        log.debug(line);
+                    }
+                }
+            }
             if (url[0].equals("POST")) {
                 int contentlength = 0;
                 while (!(line = br.readLine()).equals("")) {
+                    log.debug(line);
                     if (line.contains("Content-Length")) {
                         String[] a = line.split(" ");
                         contentlength = Integer.parseInt(a[1]);
                     }
                 }
-                requestPost(url[1], br, contentlength);
-                response302Header(dos);
+                requestPost(url[1], br, contentlength, dos);
                 return;
             }
 
@@ -84,6 +91,18 @@ public class RequestHandler extends Thread {
         }
     }
 
+    private void response302loginSuccess(DataOutputStream dos) {
+        String location = "/index.html";
+        try {
+            dos.writeBytes("HTTP/1.1 302 Redirect \r\n");
+            dos.writeBytes("Set-Cookie: logined=true \r\n");
+            log.debug("set cookie");
+            dos.writeBytes("Location: " + location);
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
 
     private void responseBody(DataOutputStream dos, byte[] body) {
         try {
@@ -101,15 +120,28 @@ public class RequestHandler extends Thread {
         return Files.readAllBytes(new File("./webapp" + url).toPath());
     }
 
-    private void requestPost(String url, BufferedReader br, int contentLength) throws IOException {
+    private void requestPost(String url, BufferedReader br, int contentLength, DataOutputStream dos) throws IOException {
+        DataBase db = DataBase.getInstance();
         if (url.equals("/user/create")) {
-            String params = IOUtils.readData(br, contentLength);
-            Map<String, String> map = HttpRequestUtils.parseQueryString(params);
+            Map<String, String> map = createMap(br, contentLength);
             User user = new User(map.get("userId"), map.get("password"), map.get("name"), map.get("email"));
-            DataBase db = DataBase.getInstance();
             db.addUser(user);
             log.debug("User : " + user);
+            response302Header(dos);
+            return;
         }
+        if (url.equals("/user/login")) {
+            Map<String, String> map = createMap(br, contentLength);
+            User user = db.findUserById(map.get("userId"));
+            if (user.getPassword().equals(map.get("password"))) {
+                response302loginSuccess(dos);
+            }
+        }
+    }
+
+    private Map<String, String> createMap(BufferedReader br, int contentLength) throws IOException {
+        String params = IOUtils.readData(br, contentLength);
+        return HttpRequestUtils.parseQueryString(params);
     }
 
 }
